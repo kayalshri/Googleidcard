a79e9ed7ceaddc9d77137ec48de3f62bf2d547eb
kayalshri <kayalshri@gmail.com>
2013-03-25 14:23:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -184,7 +184,7 @@ if(empty($code)) {
 		<script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
 		<script type="IN/Share" data-counter="right" data-url="http://ngiriraj.com/socialMedia/googleplus_idcard/'.$myFile.'"></script>
 		<!-- Place this tag where you want the share button to render. -->
-		<div class="g-plus" data-action="share" data-annotation="bubble" data-href="<?php echo $myFile; ?>"></div>
+		<div class="g-plus" data-action="share" data-annotation="bubble" data-href="http://ngiriraj.com/socialMedia/googleplus_idcard/'.$myFile.'"></div>
 		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 		<a href="https://twitter.com/share" class="twitter-share-button" data-via="kayalshri" data-url="http://ngiriraj.com/socialMedia/googleplus_idcard/'.$myFile.'" data-text="Google+ IDCARD">Tweet</a>
 		<BR>
